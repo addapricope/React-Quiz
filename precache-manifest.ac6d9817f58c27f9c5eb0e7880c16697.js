2f488aeb1d4a2377cc5a2ad7c869cd2613f5d405
addapricope <114485759+addapricope@users.noreply.github.com>
2023-01-02 13:26:05 +0200

    Updates

---
+++ precache-manifest.ac6d9817f58c27f9c5eb0e7880c16697.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd53f10c4f8d17fc02db701b4dbe715",
+    "url": "/React-Quiz/index.html"
+  },
+  {
+    "revision": "070e994772de83a54c3d",
+    "url": "/React-Quiz/static/css/main.bec98e22.chunk.css"
+  },
+  {
+    "revision": "100559fbe9989eaa1530",
+    "url": "/React-Quiz/static/js/2.cd18f9c7.chunk.js"
+  },
+  {
+    "revision": "070e994772de83a54c3d",
+    "url": "/React-Quiz/static/js/main.b94f5af2.chunk.js"
+  },
+  {
+    "revision": "a9a6941455bb720fa5d8",
+    "url": "/React-Quiz/static/js/runtime-main.6ea5c678.js"
+  }
+]);
